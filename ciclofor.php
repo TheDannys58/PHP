8e83c47bcac5414aab93b80584b1bd9da71354d8
dannys <dannygamer57@gmail.com>
2022-07-18 16:00:02 -0500

    desarrollo:ejercicios php

---
+++ ciclofor.php
@@ -0,0 +1,39 @@
+<?php
+
+ //ciclo for
+ /*
+ echo "jelouda danny";
+ echo "jelouda danny";
+ echo "jelouda danny";
+ echo "jelouda danny";
+ echo "jelouda danny";
+ echo "jelouda danny";
+ echo "jelouda danny";
+ echo "jelouda danny";
+ echo "jelouda danny";
+ echo "jelouda danny";
+ */
+  
+
+ for ($i=0; $i<=10; $i++) {
+    echo "jelouda danny <br/>";
+ }
+ //numero del 1 al 20 
+ for ($numero=1; $numero<=20; $numero++) {
+    echo $numero . "<br/>";
+ }
+ 
+ //numero del 1 al 30 en 2 en 2
+ for ($numero=1; $numero<=20; $numero=$numero+2) {
+   echo $numero . "<br/>"; 
+ }
+ echo "-----------------------";
+
+  //numerosdel 1 al 30 de 2 en 2
+ for ($numero=1; $numero<=20; $numero+=2) {
+   echo $numero . "<br/>"; 
+ }
+
+ 
+
+?>
---
+++ variable.php
@@ -0,0 +1,25 @@
+<?php
+
+  //esto es el coomentario...
+
+
+
+
+
+ //mi primera imprecion
+ echo "Hola :3";
+ echo "<br/>";
+
+ $nombre = "Dannys ";
+ $apellido_Usuario = "Borrero ";
+ $edad = 18 ;  
+
+
+ echo $nombre;
+ echo $apellido_Usuario;
+ echo $edad;
+
+ //contenacion
+ $imprimir = $nombre."  ".$apellido_Usuario."  ".$edad;
+
+?>
